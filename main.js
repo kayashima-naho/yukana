c46950fc63be56eed96295332658389d7b70c328
kayashima-naho <git@github.com:kayashima-naho/yukana.git>
2023-05-25 11:06:34 +0900

    レスポンシブ

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const openbtn1 = document.getElementById('js-openbtn');
-const menu = document.getElementById('js-menu');
-openbtn1.addEventListener('click', function() {
-  openbtn1.classList.toggle('active');
-  menu.classList.toggle('active')
-})
+const openbtn1 = document.getElementById("js-openbtn");
+
+const menu = document.getElementById("js-menu");
+console.log(menu);
+
+openbtn1.addEventListener("click", function () {
+	openbtn1.classList.toggle("active");
+	menu.classList.toggle("active");
+});
